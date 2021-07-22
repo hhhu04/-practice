c4c637498735a749654704a150eb49e436cee33a
hhhu04 <hhhu04@gmail.com>
2021-07-22 16:27:56 +0900

    a

---
+++ security/src/main/java/com/example/security/controller/SampleController.java
@@ -0,0 +1,40 @@
+package com.example.security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+@Controller
+public class SampleController {
+
+    @GetMapping("/")
+    public String index(Model model,Principal principal){
+        if(principal == null){
+            model.addAttribute("message","customer");
+        }
+        else {
+            model.addAttribute("message", "hi");
+        }
+        return "index";
+    }
+
+    @GetMapping("/info")
+    public String info (Model model){
+        model.addAttribute("message","hi info");
+        return "info";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model, Principal principal){
+        model.addAttribute("message","hi"+principal.getName());
+        return "dashboard";
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model, Principal principal){
+        model.addAttribute("message","hi admin"+principal.getName());
+        return "admin";
+    }
+}
