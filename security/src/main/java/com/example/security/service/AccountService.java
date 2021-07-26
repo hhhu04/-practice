74d4636184452166018d16b9ce880a669f374bb6
hhhu04 <hhhu04@gmail.com>
2021-07-26 16:59:08 +0900

    시큐일부

---
+++ security/src/main/java/com/example/security/service/AccountService.java
@@ -0,0 +1,41 @@
+package com.example.security.service;
+
+import com.example.security.account.Account;
+import com.example.security.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.convert.ReadingConverter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AccountService implements UserDetailsService {
+
+    private final AccountRepository accountRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Account account = accountRepository.findByUsername(username);
+        if(account == null){
+            throw new UsernameNotFoundException(username);
+        }
+
+        return User.builder()
+                .username(account.getUsername())
+                .password(account.getPassword())
+                .roles(account.getRole())
+                .build();
+    }
+
+
+    public Account createNew(Account account) {
+        account.encodePassword(passwordEncoder);
+        return this.accountRepository.save(account);
+    }
+}
